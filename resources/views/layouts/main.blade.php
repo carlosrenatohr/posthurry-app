c7bc1c5602442c97cdab3a51bce6bfa5eb3f79a6
Carlos Hernandez <honchkrow1995@gmail.com>
2016-08-08 09:37:06 -0600

    Google analytics
    - code added

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -62,6 +62,16 @@
     {{--JS--}}
     <script src="{{ asset('js/all.js') }}"></script>
     @yield('others-js')
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-82146887-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
     <title>POST HURRY @yield('pageTitle', '')</title>
 </head>
 <body style="height:100%;width:100%;padding:0;margin:0">
