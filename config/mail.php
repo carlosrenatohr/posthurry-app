e0d150e596b50e6f2480a9b9d35cb7d33de129ee
Carlos Hernandez <honchkrow1995@gmail.com>
2016-10-12 22:40:19 -0600

    Forget password implementation
    - Feature completed with laravel integration
    - clean

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => 'info@getposthurry.com', 'name' => 'Customer Service'],
+    'from' => ['address' => 'info@getposthurry.com', 'name' => 'PostHurry Service'],
 
     /*
     |--------------------------------------------------------------------------
