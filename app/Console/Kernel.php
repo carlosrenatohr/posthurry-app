a7ea29972268297baf870569bbe6d059c9936819
Yoga Sukma <hello@yogasukma.web.id>
2016-10-14 00:48:39 +0800

    update scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\Ftw::class
+        Commands\Ftw::class,
         Commands\Inspire::class,
         Commands\PostPerDayChecker::class
     ];
