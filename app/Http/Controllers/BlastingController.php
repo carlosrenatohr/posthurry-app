92ee9656c53870dbab99faa09bd934c36baf81b7
Yoga Sukma <hello@yogasukma.web.id>
2016-10-19 00:13:24 +0800

    adding random code to blasting table

--- app/Http/Controllers/BlastingController.php
+++ app/Http/Controllers/BlastingController.php
@@ -32,9 +32,11 @@ class BlastingController extends Controller
     public function index() 
     {
         $user_id = Auth::user()->id;
-        $user = User::with( [ 'blasting' => function( $table ) {
-           return $table->groupBy( 'code' ); 
-        } ] )->find($user_id);
+        //$user = User::with( [ 'blasting' => function( $table ) {
+        //   return $table->groupBy( 'code' ); 
+        // } ] )->find($user_id);
+
+        $user = User::find( $user_id );
 
         return view('blasting.index', ['user' => $user]);
     }
